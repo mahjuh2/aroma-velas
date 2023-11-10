59d66e850434d2e35ac8603ccb162d6b1cd8b612
mahjuh2 <143728846+mahjuh2@users.noreply.github.com>
2023-11-10 11:17:00 -0300

    formulario do login

--- maju velas/java.js
+++ maju velas/java.js
@@ -17,3 +17,25 @@ function myFunction() {
       }
     }
   }
+
+  
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+let buttonlogin = documen.querySelector("#login")
+buttonlogin,addEventListener("click",function (event){
+  event.preventDefault();
+  console.log("Login finalizado")
+
+});
+
+titulo.addEventListener("click",function(){
+console.log("Fui logado");
+});
+
